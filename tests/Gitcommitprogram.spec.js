61c01ab70bdfba44112e9597382519762c60f348
veenasriram1 <veenasriram0601@gmail.com>
2026-03-06 08:40:57 +0530

    pushing my code

---
+++ tests/Gitcommitprogram.spec.js
@@ -0,0 +1,25 @@
+import {test, expect} from '@playwright/test'
+
+test("Test case1", async({page})=>
+{
+ console.log("this is Testcase1")
+})
+
+test("Test case 2", async({page})=>
+{
+ console.log("this is Testcase2")
+})
+
+test("Test case 3", async({page})=>
+{
+console.log(" this is Testcase3")
+})
+
+test("Test case 4", async({page})=>
+{
+ console.log("this is Testcase4")
+})
+test("Test case 5", async({page})=>
+{
+console.log("this is Testcase5")
+})
